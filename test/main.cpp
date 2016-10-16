db2c0a992f3ae904640393a716d2e3706f61c022
Barrett Davis <barrettd@thefrog.com>
2016-10-15 18:41:20 -0600

    giving clock_gettime() a try

--- test/main.cpp
+++ test/main.cpp
@@ -4,12 +4,26 @@
 //  Created by Barrett Davis on 10/12/16.
 //  Copyright © 2016 Tree Frog Software. All rights reserved.
 //
-
+#include <ctime>      // clock_gettime
 #include <iostream>
 #include "gpio.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+namespace  tfs  {
+
+
+}   // namespace tfs
+
+int main( void ) {
+    std::cout << "GPIO test application\n";
+    
+    struct timespec start;
+    struct timespec end;
+    
+    /* measure monotonic time */
+    clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+    clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+    
+//    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
     return 0;
 }
+
